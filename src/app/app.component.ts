11601ad44a730e2bf67371b949e160bdb07108ec
ahmdbourji <ahmdbourji9999@gmail.com>
2025-08-06 15:27:57 -0700

    feat: add responsive hamburger menu to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,30 @@
-//import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
-//@Component({
-  //selector: 'app-root',
-  //standalone: true,
-  //imports: [RouterOutlet],
-  //templateUrl: './app.component.html',
-  //styleUrl: './app.component.css'
-//})
-//export class AppComponent {
-//  title = 'frontend';
-//}
-// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
+
 @Component({
   standalone: true,
   selector: 'app-root',
- imports: [CommonModule, FormsModule,RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] 
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   environment = environment;
+
+  // متغير القائمة
+  menuOpen = false;
+
+  // دالة فتح/إغلاق القائمة
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // دالة إغلاق القائمة عند اختيار رابط
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
 
